ea3b2831eaa48184382443b14348243c1a0b1c23
Anthony Delgado <me@anthonydelgado.me>
2016-12-19 09:23:21 -0500

    Update PORT console log

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var PORT = process.env.PORT || '3000';
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-    // res.send('Hello World!');
     res.sendFile('/public/index.html');
 });
 
-app.listen(process.env.PORT || '3000', function () {
-    console.log('Example app listening on port 3000!')
+app.listen(PORT, function () {
+    console.log('React app listening on port ' + PORT + '!')
 });
